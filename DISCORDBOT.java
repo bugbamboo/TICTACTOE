cd58abcfefd659edc4adbd13d11fc53d1a8f4673
bugbamboo <39929404+bugbamboo@users.noreply.github.com>
2020-05-20 20:39:54 -0400

    Update DISCORDBOT.java

--- DISCORDBOT.java
+++ DISCORDBOT.java
@@ -15,7 +15,7 @@ import java.util.*;
 public class hi {
 
     public static void main(String[] args) throws javax.security.auth.login.LoginException, IOException {
-        JDA jda = new JDABuilder("NzA3NjI5Mjk1MTUzMDUzNzY4.XsSaMg.WHBIN2BgwyEWwxPToA3VtlC4ghE").build();
+        JDA jda = new JDABuilder("").build();
         jda.addEventListener(new TICTACTOE());
     }
 }
